02733be8f6aabb24b840693f9de72a0d28f14c23
Brian Gould <brian.gould@gmail.com>
2020-09-06 15:27:11 -0400

    Lesson 67 Done. Won't start after game over

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     
-   public void LoadStartMenu() 
+   public void StartGame() 
    {
        SceneManager.LoadScene(0);
    }
