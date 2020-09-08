efc93bf96bbe7308faf1e53b3e11bb59455f81ec
Brian Gould <brian.gould@gmail.com>
2020-09-07 23:01:55 -0400

    Lesson 76 done. Number of Hits will not update

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,22 +2,60 @@
 
 public class Block : MonoBehaviour
 {
+    //Config
     [SerializeField] private AudioClip breakSound = null;
     [SerializeField] private GameObject blockSparklesVFX = null;
+    [SerializeField] private int maxHits;
+    [SerializeField] private Sprite[] hitSprites;
+    
     //Cached References
     private Level _level;
     private GameStatus _gameStatus;
+    
+    //State Variables
+    [SerializeField] private int timesHit;
 
     private void Start()
-    { 
-         _level = FindObjectOfType<Level>();
-        _level.CountBreakableBlocks();
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        _level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            _level.CountBlocks();
+        }
         _gameStatus = FindObjectOfType<GameStatus>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+       
+    }
+    
+     private void ShowNextHitSprite()
+     {
+         int spriteIndex = timesHit - 1;
+       GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
     }
 
     private void DestroyBlock()
@@ -25,7 +63,7 @@ public class Block : MonoBehaviour
         PlayBreakSound();
         RemoveBlock();
         TriggerSparklesVFX();
-    }
+        }
 
     private void RemoveBlock()
     {
