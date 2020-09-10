a0dac91787c1db48423d89ab9cf1b46041a25a80
Brian Gould <brian.gould@gmail.com>
2020-09-10 12:55:29 -0400

    Updated Logic

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits = 16f;
     [SerializeField] private float minX = 1f;
     [SerializeField] private float maxX = 15f;
-
     
     private GameStatus _gameStatus;
     private Ball _ball;
@@ -19,12 +17,14 @@ public class Paddle : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
         paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
     }
+    
+
 
     private float GetXPos()
     {
